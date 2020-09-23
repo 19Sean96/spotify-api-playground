8f775d996dcab77af27ca16036ca01259c2f8983
Magic-Mayo <mike.mayoii@gmail.com>
2020-09-23 00:44:53 -0700

    moved loggedIn state here from app

--- public/front-end/src/context/index.js
+++ public/front-end/src/context/index.js
@@ -6,17 +6,20 @@ export const WebPlayer = createContext(null)
 export default ({children}) => {
     const [tokens, setTokens] = useState([]);
     const [player, setPlayer] = useState({});
+    const [loggedIn, setLoggedIn] = useState(false);
 
     useEffect(() => {
+        console.log('context')
         if(window.Spotify){
             const playerInit = window.onSpotifyWebPlaybackSDKReady;
             playerInit(tokens[0])
             setPlayer(window.webPlayer)
+            setLoggedIn(true);
         }
     }, [tokens])
 
     return (
-        <WebPlayer.Provider value={{tokens, player, setPlayer, setTokens}}>
+        <WebPlayer.Provider value={{loggedIn, tokens, player, setLogin: setLoggedIn, setPlayer, setTokens}}>
             {children}
         </WebPlayer.Provider>
     )
