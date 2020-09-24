c332385685ed9024704843fc7dd64d37cb0047d8
Sean <1996.sean.alexander@gmail.com>
2020-09-23 21:39:06 -0700

    migrated Spotify webPlayer init from html script to ReactContext

--- public/front-end/src/context/index.js
+++ public/front-end/src/context/index.js
@@ -1,25 +1,74 @@
 import React, { createContext, useEffect, useState } from 'react';
 
 
-export const WebPlayer = createContext(null)
+export const WebPlayerContext = createContext(null)
 
 export default ({children}) => {
     const [tokens, setTokens] = useState([]);
-    const [player, setPlayer] = useState({});
+    const [player, setPlayer] = useState();
     const [loggedIn, setLoggedIn] = useState(false);
+    const [connected, setConnected] = useState(false);
+
+    const handleSDKLoaded = () => {
+        return new Promise((resolve) => {
+            if (window.Spotify) {
+                resolve();
+            } else {
+                window.onSpotifyWebPlaybackSDKReady = resolve;
+            }
+        });
+    }
 
     useEffect(() => {
-        if(window.Spotify){
-            const playerInit = window.onSpotifyWebPlaybackSDKReady;
-            playerInit(tokens[0])
-            setPlayer(window.webPlayer)
-            setLoggedIn(true);
+        console.log(tokens);
+        console.log('setting player')
+        if (tokens.length > 1) {
+            handleSDKLoaded().then(() => {
+                setPlayer(prevPlayer =>
+                    prevPlayer ||
+                    new window.Spotify.Player({
+                        name: 'Chromesthetics',
+                        getOAuthToken: cb => cb(tokens[0])
+                    })
+                )
+                setLoggedIn(true);
+            })
         }
     }, [tokens])
 
+    useEffect(() => {
+        if(player){
+            // Error handling
+            player.addListener('initialization_error', ({ message }) => { console.error(`init error: ${message}`); });
+            player.addListener('authentication_error', ({ message }) => { console.error(`auth error: ${message}`); });
+            player.addListener('account_error', ({ message }) => { console.error(`account error: ${message}`); });
+            player.addListener('playback_error', ({ message }) => { console.error(`play error: ${message}`); });
+
+            // Playback status updates
+            player.addListener('player_state_changed', state => {
+                setConnected(state !== null ? true : false);
+            });
+
+            // Ready
+            player.addListener('ready', ({ device_id }) => {
+                console.log('Ready with Device ID', device_id);
+            });
+
+            // Not Ready
+            player.addListener('not_ready', ({ device_id }) => {
+                console.log('Device ID has gone offline', device_id);
+            });
+
+            // Connect to the player!
+            player.connect().then(connected => {
+                console.log(connected);
+            });
+        }
+    }, [player])
+
     return (
-        <WebPlayer.Provider value={{loggedIn, tokens, player, setLogin: setLoggedIn, setPlayer, setTokens}}>
+        <WebPlayerContext.Provider value={{connected, loggedIn, tokens, player, setLoggedIn, setPlayer, setTokens, setConnected}}>
             {children}
-        </WebPlayer.Provider>
+        </WebPlayerContext.Provider>
     )
 }
