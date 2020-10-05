db98bb0793c754d4b9906b38d8a1112683e1a6b8
Sean <1996.sean.alexander@gmail.com>
2020-10-05 14:49:44 -0700

    removed dev files

---
+++ public/front-end/build/precache-manifest.286eff4594576661026345bb3be9664d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118d0ba2102a7ba16d47e1826145479c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75c9299814ff3b12f266",
+    "url": "/static/css/main.3e8484cd.chunk.css"
+  },
+  {
+    "revision": "e1555ba0963bb87e9044",
+    "url": "/static/js/2.482c0ae2.chunk.js"
+  },
+  {
+    "revision": "1fb0026f06713a4b6d0c48b18c6e7e94",
+    "url": "/static/js/2.482c0ae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c9299814ff3b12f266",
+    "url": "/static/js/main.11941d0e.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
